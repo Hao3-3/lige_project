c2c0b8e9889181e35da137055d9873a614f2641b
blalG <3162026874@qq.com>
2026-09-07 15:42:38 +0800

    创建 mock.js

---
+++ mock.js
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>智慧城市-济南</title>
+  <!-- 地图导航插件（Mapbox Directions） -->
+  <script src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.js"></script>
+  <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.1/mapbox-gl-directions.css" type="text/css">
+  <!-- 复制阿里巴巴图标库链接 -->
+  <link rel="stylesheet" href="https://at.alicdn.com/t/c/font_4072822_j5r3vfaxh8h.css">
+  <style>
+    .mapboxgl-ctrl-top-right .mapboxgl-ctrl {
+  margin: 10px 5px 0 0;
+  float: right;
+  margin-top: 50px;
+}
+  </style>
+</head>
+
+<body>
+  <div id="app"></div>
+  <script type="module" src="/src/main.js"></script>
+</body>
+
+</html>
